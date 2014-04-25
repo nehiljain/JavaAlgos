36c1e213e8bc7e1e3bd4f57863af06ed61baeda1
nehiljain <jain.nehil@gmail.com>
2014-04-25 12:10:07 -0300

    solution using bitwise operations

--- pkg/nehil/chp1/P1.java
+++ pkg/nehil/chp1/P1.java
@@ -5,7 +5,7 @@ package pkg.nehil.chp1;
 public class P1 {
 	
 	private String testStringUnique = "Uniqe";
-	private String testStringNotUnique = "unique";
+	private String testStringNotUnique = "@uniquasdfghjkl;@e";
 
 	/**
 	 * This assumes that the string is made of 256 ascii characters only
@@ -26,10 +26,27 @@ public class P1 {
 		return true;
 	}
 	
+	private boolean isUniqueCharAll2(String str) {
+		int check = 0;
+		
+		for (int i = 0; i <= str.length() - 1; i++) {
+			char c = str.charAt(i);
+			if ((check & (1 << c)) > 0) {
+				return false;
+			}
+			check = check | (1 << c);
+		}
+		
+		return true;
+		
+	}
+	
 	public static void main(String argsp[]) {
 		P1 obj = new P1();
 		System.out.println("Unique string == " + obj.isUniqueCharAll(obj.testStringUnique));
 		System.out.println("Unique string == " + obj.isUniqueCharAll(obj.testStringNotUnique));
+		System.out.println("Unique string == " + obj.isUniqueCharAll2(obj.testStringUnique));
+		System.out.println("Unique string == " + obj.isUniqueCharAll2(obj.testStringNotUnique));
 		
 	}
 }
