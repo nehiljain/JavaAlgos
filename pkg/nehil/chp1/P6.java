f5b470ca0fc0cc69a52439ebb0d400c0069d6871
nehiljain <jain.nehil@gmail.com>
2014-04-27 10:57:57 -0300

    Problem 6 completed

--- pkg/nehil/chp1/P6.java
+++ pkg/nehil/chp1/P6.java
@@ -1,7 +1,42 @@
 package pkg.nehil.chp1;
 
 public class P6 {
+	
+	public void rotate90(String[][] s, int n) {
+		System.out.println("\n\n");
+		String temp1, temp2;
+		int index = 0;
+		while (index <= n/2) {
+			for (int i = index; i <= n-2-index; i++) {
+//				System.out.println(Integer.toString(i) + "th item:: " + s[index][i] + 
+//						" - >" + s[i][n-1-index]  + 
+//						" - >" + s[n-1-index][n-i-1] + 
+//						" - >"  + s[n-i-1][index]);
+				temp1 = s[i][n-1-index];
+				s[i][n-index-1] = s[index][i];
+				temp2 = s[n-1-index][n-i-1];
+				s[n-1-index][n-i-1] = temp1;
+				temp1 = s[n-i-1][index];
+				s[n-i-1][index] = temp2;
+				s[index][i] = temp1;
+			}
+			index++;
+		}
+		
+	}
+	
+	private void printStringArray(String[][] s, int n) {
 
+		
+		for (int i = 0; i <= n-1; i++) {
+			System.out.println("");
+			for (int j = 0; j <= n-1; j++) {
+				System.out.print(s[i][j]+"  ");
+			}
+			System.out.println("");
+		}
+	}
+	
 	public static void main(String args[]) {
 		String test0[][] = {};
 		String test1[][] = {{"1"}};
@@ -17,8 +52,12 @@ public class P6 {
 		String test4[][] = {
 				{"1","2","3","4"},
 				{"5","6","7","8"},
-				{"9","10","11","12"}
+				{"9","10","11","12"},
+				{"13","14","15","16"}
 		};
-		
+		P6 obj = new P6();
+		obj.printStringArray(test4, 4);
+		obj.rotate90(test4, 4);
+		obj.printStringArray(test4, 4);
 	}
 }
