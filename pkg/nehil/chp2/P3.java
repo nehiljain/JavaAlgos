6a2bc6f6d5b0c21ca4031185309404b846d1bd86
nehiljain <jain.nehil@gmail.com>
2014-05-07 20:33:40 -0300

    2.3 solved

--- pkg/nehil/chp2/P3.java
+++ pkg/nehil/chp2/P3.java
@@ -1,6 +1,5 @@
 package pkg.nehil.chp2;
 
-
 import pkg.nehil.linkedlist.*;
 
 
@@ -8,9 +7,31 @@ import pkg.nehil.linkedlist.*;
 public class P3 extends SingleLinkedList {
 
 
+	public void removeNode(ListNode node){
+		if(node == null) System.out.println("Failure");
+
+		node.item = node.next.item;
+		node.next = node.next.next;
+	}
+
+
 	public static void main(String args[])  {
 
-		P3 obj = new P3();
 		
+		System.out.println("Problem 2 from cracking the coding interview");
+
+		P3 obj = new P3();
+		for (int i = 1; i <= 10; i++) {
+			obj.insertFront(i);
+		}
+
+		ListNode curNode = obj.getHead();
+		while (curNode.item != 4) {
+			curNode = curNode.next;
+		}
+
+		obj.printList(obj.getHead());
+		obj.removeNode(curNode);
+		obj.printList(obj.getHead());
 	}
 }
