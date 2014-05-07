fd76d8e0e6e6af878de3c7532a2c705658cd1ae3
nehiljain <jain.nehil@gmail.com>
2014-05-06 21:11:21 -0300

    Started solving problem 3 of cracking the coding interview

---
+++ pkg/nehil/chp2/P3.java
@@ -0,0 +1,16 @@
+package pkg.nehil.chp2;
+
+
+import pkg.nehil.linkedlist.*;
+
+
+
+public class P3 extends SingleLinkedList {
+
+
+	public static void main(String args[])  {
+
+		P3 obj = new P3();
+		
+	}
+}
