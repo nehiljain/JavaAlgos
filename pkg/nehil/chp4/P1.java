e4a7230b348785bfd3517e3a6c70655a00cf565e
nehiljain <jain.nehil@gmail.com>
2014-05-22 23:01:40 -0300

    untested, code but conceptually correct solution P1, chp4

--- pkg/nehil/chp4/P1.java
+++ pkg/nehil/chp4/P1.java
@@ -6,6 +6,11 @@ public class P6<Key, Value> {
 
 	private Node root;
 
+	public P6() {
+
+		root = new Node()
+	}
+
 	private class Node {
 		private Key key;
 		private Value value;
@@ -18,7 +23,28 @@ public class P6<Key, Value> {
 		}
 	}
 
+	private static int getMinDepth(Node node) {
+		if (node == null) return 0;
 
+		return 1 + Math.min(getMinDepth(node.left), getMinDepth(node.right));
+	}
+
+	private static int getMaxDepth(Node node) {
+		if (node == null) return 0;
+
+		return 1 + Math.max(getMaxDepth(node.left), getMaxDepth(node.right));
+ 	}
+
+ 	public boolean isBalanced() {
+ 		return (getMaxDepth(this.root) - getMinDepth(this.root) <= 1 );
+  	}
+
+
+  	public static void main(String args[]) {
+  		P6<Integer, Integer> obj = new P6<Integer, Integer>();
+
+
+  	}
 
 
 
